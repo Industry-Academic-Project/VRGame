a67c51187a13890960776c6a87b54193dba2c1e6
aprnald74 <a010313813321@gmail.com>
2024-04-30 18:02:24 +0900

    ✨ 미니맵 구현

---
+++ VRGame/Assets/Scripts/UI/UIPlayerTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPlayerTest : MonoBehaviour
+{
+    // 이동 속도
+    public float speed = 5f;
+
+    // 플레이어 Rigidbody
+    public Rigidbody _rigidbody;
+    
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // 사용자의 입력에 따라 전후좌우로 이동하고 싶다.]
+        // 1. 사용자의 입력을 받는다.
+        float h = ARAVRInput.GetAxis("Horizontal");
+        float v = ARAVRInput.GetAxis("Vertical");
+        
+        // 2. 이동한다.
+        transform.Translate(new Vector3(h, 0f, v).normalized * 0.1f);
+
+        if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) return;
+    }
+}
