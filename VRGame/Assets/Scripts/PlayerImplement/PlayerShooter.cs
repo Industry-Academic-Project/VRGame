64cfdcf67e48bdc590e521d13f87e095e2134efe
ming10 <jangminguo@gmail.com>
2024-05-07 17:37:05 +0900

    :sparkles: 총알 추가

---
+++ VRGame/Assets/Scripts/PlayerImplement/PlayerShooter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public GameObject bulletPrefab; // 총알 프리팹
+    public Transform shootingPosition; // 발사 위치
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger,OVRInput.Controller.RTouch)) // 오른쪽 버튼
+        {
+            //총알 발사 입력
+            //Instantiate(bulletPrefab, shootingPosition, Quaternion.identity);
+        }
+    }
+}
